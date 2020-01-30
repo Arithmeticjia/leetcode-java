1525269b63e0f5e55f297e45f582f9b4b87cceb8
ArithmeticJia <1097197237@qq.com>
2020-01-30 12:35:31 +0800

    add multiply

---
+++ src/com/company/multiply.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class multiply {
+    /*
+     * 计算形式
+     *    num1
+     *  x num2
+     *  ------
+     *  result
+     */
+
+    public String multiply(String num1,String num2){
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        String res = "0";
+        
+        return res;
+    }
+}

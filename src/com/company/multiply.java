32cffa13ddb1c296999c107aa6e9c70d50a3e09a
ArithmeticJia <1097197237@qq.com>
2020-02-05 16:09:17 +0800

    add isValidSudoku

--- src/com/company/multiply.java
+++ src/com/company/multiply.java
@@ -29,7 +29,6 @@ public class multiply {
             // char ''
             // String ""
             int n1Val = num1.charAt(i) - '0';
-            System.out.println(n1Val);
             int carry = 0;
             // cur存放第一行每一个数和第二行的每一个数的乘积的值，包括进位
             // StringBuilder存储字符串
@@ -47,21 +46,24 @@ public class multiply {
             System.out.println(cur);
             // 837 516 294
             results[i] = cur.reverse().append(generateZero(num1.length() - i - 1)).toString();
+            System.out.println(results[i]);
             // 738 6150 49200
         }
 
-        String res = results[0];
-        for(int i = 1; i < results.length; i ++) {
-            res = addStrings(res, results[i]);
-        }
+            String res = results[0];
+            for(int i = 1; i < results.length; i ++) {
+                res = addStrings(res, results[i]);
+            }
 
-        return res;
+            return res;
 }
     // 补0
     private String generateZero(int n) {
         char[] zeros = new char[n];
         Arrays.fill(zeros, '0');
-        return new String(zeros);
+        // String to char
+//         return new String(zeros);
+        return  String.valueOf(zeros);
     }
 
     private String addStrings(String num1, String num2) {
@@ -69,6 +71,7 @@ public class multiply {
         int carry = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
+        // 两个数从最右边开始，直到位数较大的也为0为止
         while (i >= 0 || j >= 0 || carry != 0) {
             int num1Val = i >= 0 ? num1.charAt(i) - '0' : 0;
             int num2Val = j >= 0 ? num2.charAt(j) - '0' : 0;
@@ -80,7 +83,7 @@ public class multiply {
             if (i >= 0) i--;
             if (j >= 0) j--;
         }
-
+        // String
         return res.reverse().toString();
     }
 }

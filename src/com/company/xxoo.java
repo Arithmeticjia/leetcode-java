650173a817e8d032aeb1a4a7e3c69d125952f0df
ArithmeticJia <1097197237@qq.com>
2020-03-25 16:21:00 +0800

    add sth

--- src/com/company/xxoo.java
+++ src/com/company/xxoo.java
@@ -31,7 +31,7 @@ public class xxoo {
                 }
             }
         }
-
+        // 把不是'#‘的'O'改成'X'
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 'O') {
@@ -44,7 +44,7 @@ public class xxoo {
         }
 
     }
-
+    // dfs找和边界联通的'O'并变成'#'，最后不能被'X'替换，并且下一次就不会被搜索
     public static void dfs(char[][] board,int i,int j) {
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
             // board[i][j] == '#' 说明已经搜索过了.

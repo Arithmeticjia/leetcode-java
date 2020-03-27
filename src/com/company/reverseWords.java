4eaf771b420018cc81ee7f79ab5faaa42da77667
ArithmeticJia <1097197237@qq.com>
2020-03-27 23:31:10 +0800

    add sth

--- src/com/company/reverseWords.java
+++ src/com/company/reverseWords.java
@@ -4,21 +4,33 @@ import java.util.Arrays;
 
 public class reverseWords {
 
-    public String reverseWords(String s){
+    public static void main(String[] args){
+        reverseWords test = new reverseWords();
+        String t = " I love u  ";
+        String k = " ";
+        String m = "the sky is blue";
+        System.out.print(reverseWords(m));
+    }
+
+    public static String reverseWords(String s){
 
         if(s == null){
             return "";
         }
+        // 如果全是空格，根据测试用例返回""或者" "
+        if("".equals(s.trim())){
+            return s;
+        }
         String[] words = s.split(" ");
-        System.out.print(Arrays.toString(words));
         StringBuilder ans = new StringBuilder();
-        for(int i = 0;i < words.length;i++){
+        for(int i = words.length - 1;i >= 0;i--){
             if("".equals(words[i]) ){
                 continue;
             }
             ans.append(words[i]);
             ans.append(" ");
         }
+        // 把最后一个加上的空格" "去掉
         return ans.toString().trim();
     }
 }

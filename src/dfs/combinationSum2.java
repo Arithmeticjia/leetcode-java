2b4b2ec8999e7549adb3309ba7ecb023c9a9d7dd
ArithmeticJia <1097197237@qq.com>
2020-04-20 17:43:22 +0800

    add few

--- src/dfs/combinationSum2.java
+++ src/dfs/combinationSum2.java
@@ -6,26 +6,25 @@ public class combinationSum2 {
 
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        if(candidates == null || candidates.length == 0) {
+        if(candidates.length == 0|| candidates == null){
             return ans;
         }
         Arrays.sort(candidates);
-        helper(candidates,target,0,0,new ArrayList<>());
+        dfs(candidates,target,0,0,new ArrayList<>());
         return ans;
     }
-    public void helper(int[] candidates,int target,int sum,int index,List<Integer> temp){
+    public void dfs(int[] candidates,int target,int sum,int index,List<Integer> temp){
         if(sum == target){
-            ans.add(new ArrayList<>(temp));
+            ans.add(new ArrayList(temp));
             return;
         }else{
-            for(int i = index; i < candidates.length; i++){
-                // 必须先判断是否重复
-                if(i > index && candidates[i] == candidates[i-1]) {
-                    continue; //continue的原因在于当前数和上一个数字大小相同，那么上个数字出现的符合预期的所有结果会包含这个数字出现的结果里。
+            for(int i = index;i < candidates.length;i++){
+                if(i > index && candidates[i] == candidates[i-1]){
+                    continue;
                 }
                 if(sum + candidates[i] <= target){
                     temp.add(candidates[i]);
-                    helper(candidates,target,sum+candidates[i],i+1,temp);
+                    dfs(candidates,target,sum+candidates[i],i+1,temp);
                     temp.remove(temp.size()-1);
                 }
             }

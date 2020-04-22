40bc57c6dfae55a0eaaa0a81f9de6a5b7e5ed12d
ArithmeticJia <1097197237@qq.com>
2020-04-22 17:04:41 +0800

    add few

---
+++ src/sort/method/quickSortMy.java
@@ -0,0 +1,53 @@
+package sort.method;
+
+import java.util.Arrays;
+
+public class quickSortMy {
+
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            //找出基准
+            int partition = partition(arr, left, right);
+            //分成两边递归进行
+            quickSort(arr, 0, partition - 1);
+            quickSort(arr, partition + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        // 基准取左边第一个
+        int pivot = arr[left];
+        // 左指针
+        int low = left;
+        // 右指针
+        int high = right;
+        while(low != high){
+
+            while (low < high && arr[high] > pivot) {
+                high--;
+            }
+            while (low < high && arr[low] <= pivot) {
+                low++;
+            }
+            //找到low比基准大，high比基准小，进行交换
+            if (low < high) {
+                swap(arr, low, high);
+            }
+        }
+        // 交换基准和low
+        swap(arr,left,low);
+        return low;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 4, 6, 1, 3, 7, 9, 8, 5};
+        quickSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+}
---
+++ src/sort/method/quickSortZhiHu.java
@@ -0,0 +1,57 @@
+package sort.method;
+
+import java.util.Arrays;
+
+/**
+ * 左边开始比基准大，停下
+ * 右边开始比基准小，停下
+ * 交换
+ * 退出while后把left和基准交换，左边小于基准，右边大于基准
+ * 递归
+ */
+public class quickSortZhiHu {
+    public static void quickSort(int[] arr, int startIndex, int endIndex) {
+        if (startIndex < endIndex) {
+            //找出基准
+            int partition = partition(arr, startIndex, endIndex);
+            //分成两边递归进行
+            quickSort(arr, startIndex, partition - 1);
+            quickSort(arr, partition + 1, endIndex);
+        }
+    }
+
+    //找基准
+    private static int partition(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[startIndex];
+        int left = startIndex;
+        int right = endIndex;
+        while (left != right) {
+            while (left < right && arr[right] > pivot) {
+                right--;
+            }
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+            //找到left比基准大，right比基准小，进行交换
+            if (left < right) {
+                swap(arr, left, right);
+            }
+        }
+        //第一轮完成，让left和right重合的位置和基准交换，返回基准的位置
+        swap(arr, startIndex, left);
+        return left;
+    }
+
+    //两数交换
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 4, 6, 1, 3, 7, 9, 8, 5};
+        quickSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
+    }
+}

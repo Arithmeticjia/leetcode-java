7590a78ac1b04b7db724c86071102e71066f0bdb
ArithmeticJia <1097197237@qq.com>
2020-05-08 15:43:55 +0800

    add destCity

---
+++ src/com/company/destCity.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class destCity {
+    public String destCity(List<List<String>> paths){
+        HashSet<String> set = new HashSet<>();
+        for(List<String> path:paths){
+            set.add(path.get(0));
+        }
+        for(List<String> path:paths){
+            if(!set.contains(path.get(1))){
+                return path.get(1);
+            }
+        }
+        return null;
+    }
+}

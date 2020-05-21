133030d1f8a50b85ebc123ef43fe80ce4a011f7a
ArithmeticJia <1097197237@qq.com>
2020-05-22 05:50:34 +0800

    add prefixesDivBy5.java

--- src/com/company/prefixesDivBy5.java
+++ src/com/company/prefixesDivBy5.java
@@ -7,17 +7,16 @@ public class prefixesDivBy5 {
 
     public List<Boolean> prefixesDivBy5(int[] A){
         List<Boolean> list =  new ArrayList<>();
-        int sum = 0;
+        int tail = 0;
         for(int i = 0;i < A.length;i++){
-            sum += A[i];
-            sum = sum > 9 ? sum - 10: sum;
-            if(sum == 0 || sum == 5){
+            // 算出最后一位即可
+            tail = tail * 2 + A[i];
+            tail = tail > 9 ? tail - 10: tail;
+            if(tail == 0 || tail == 5){
                 list.add(true);
             }else {
                 list.add(false);
             }
-            // 算出当前的二进制对应的十进制
-            sum = sum * 2;
         }
 
         return list;

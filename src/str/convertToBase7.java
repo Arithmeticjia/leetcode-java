d3c62bc86b25dcd31cd2a035b68cfa16988a7936
ArithmeticJia <1097197237@qq.com>
2020-05-24 14:36:34 +0800

    add convertToBase7.java

---
+++ src/str/convertToBase7.java
@@ -0,0 +1,28 @@
+package str;
+
+public class convertToBase7 {
+
+    public static void main(String[] args) {
+        System.out.println(convertToBase7.convertToBase7(0));
+    }
+
+    public static String convertToBase7(int num){
+        String ans = "";
+
+        if(num < 0){
+            num = -num;
+            ans += "-";
+        }else if (num == 0){
+            ans = "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (num > 0){
+            int mod = num % 7;
+            sb.append((char)(mod + 48));
+            num = num / 7;
+        }
+        ans += sb.reverse().toString();
+        return ans;
+    }
+}

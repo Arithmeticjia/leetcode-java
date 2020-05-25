9732f2833c0a26418f163c1cff8ecf0414f7e881
ArithmeticJia <1097197237@qq.com>
2020-05-25 10:29:48 +0800

    add numberOfArithmeticSlices.java dp

--- src/com/company/numberOfArithmeticSlices.java
+++ src/com/company/numberOfArithmeticSlices.java
@@ -14,6 +14,19 @@ public class numberOfArithmeticSlices {
         numberOfArithmeticSlices.numberOfArithmeticSlices(A);
     }
 
+    public int numberOfArithmeticSlicesDP(int[] A) {
+        int[] dp = new int[A.length];
+        int sum = 0;
+        for (int i = 2; i < dp.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                dp[i] = 1 + dp[i - 1];
+                sum += dp[i];
+            }
+        }
+        return sum;
+    }
+
+
     public static int numberOfArithmeticSlices(int[] A) {
         // 计数
         int count = 0;

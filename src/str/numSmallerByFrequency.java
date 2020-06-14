5153cba14640aefa612c725f23454bef9fb5acf1
ArithmeticJia <1097197237@qq.com>
2020-06-14 15:15:08 +0800

    add numSmallerByFrequency.java

--- src/str/numSmallerByFrequency.java
+++ src/str/numSmallerByFrequency.java
@@ -2,6 +2,20 @@ package str;
 
 import java.util.Arrays;
 
+/**
+ * 1170. 比较字符串最小字母出现频次
+ * 我们来定义一个函数 f(s)，其中传入参数 s 是一个非空字符串；该函数的功能是统计 s  中（按字典序比较）最小字母的出现频次。
+ *
+ * 例如，若 s = "dcce"，那么 f(s) = 2，因为最小的字母是 "c"，它出现了 2 次。
+ *
+ * 现在，给你两个字符串数组待查表 queries 和词汇表 words，请你返回一个整数数组 answer 作为答案，
+ * 其中每个 answer[i] 是满足 f(queries[i]) < f(W) 的词的数目，W 是词汇表 words 中的词。
+ *
+ * 输入：queries = ["bbb","cc"], words = ["a","aa","aaa","aaaa"]
+ * 输出：[1,2]
+ * 解释：第一个查询 f("bbb") < f("aaaa")，第二个查询 f("aaa") 和 f("aaaa") 都 > f("cc")。
+ *
+ */
 public class numSmallerByFrequency {
 
     public static void main(String[] args) {

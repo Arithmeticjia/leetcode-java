2930ed3995b66bf75d87b7319da97810d30efb5a
ArithmeticJia <1097197237@qq.com>
2020-06-15 23:46:35 +0800

    add allstates threads

--- src/threads/AllState.java
+++ src/threads/AllState.java
@@ -9,11 +9,12 @@ public class AllState extends Thread{
     public void run() {
         for(int i = 0;i < 10;i++){
             try {
+                // sleep() 使当前线程进入阻塞状态，在指定时间不会执行
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println(getName() + i);
+            System.out.println(getName() + " " + i);
         }
 
     }
@@ -30,21 +31,22 @@ public class AllState extends Thread{
         //观察状态
         state = t.getState();
         // RUNNABLE
+        // 此时t线程进入RUNNABLE状态
         System.out.println(state);
 
         while(state != State.TERMINATED){
             // 活动的线程数
             int num = Thread.activeCount();
-            System.out.println(num);
+            System.out.println("活动的线程数" + " " + num);
             try {
-                // sleep() 使当前线程进入阻塞状态，在指定时间不会执行
-                AllState.sleep(200);
+                // 100ms监控一次
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            // 此时t线程被阻塞
+            // 此时t线程TIMED_WAITING或者RUNNABLE
             state = t.getState();
-            // TIMED_WAITING
+            // TIMED_WAITING或者RUNNABLE
             System.out.println(state);
         }
     }

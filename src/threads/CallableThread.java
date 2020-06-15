a1bee4fb9ee2cf6a70c98dfb2b3c7b9c0785a193
ArithmeticJia <1097197237@qq.com>
2020-06-15 10:49:08 +0800

    add three methods of starting threads

--- src/threads/CallableThread.java
+++ src/threads/CallableThread.java
@@ -1,10 +1,37 @@
 package threads;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public class CallableThread implements Callable {
+
+
+    public static void main(String[] args) {
+        CallableThread ct = new CallableThread();
+        FutureTask<Integer> ft = new FutureTask<>(ct);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " 的循环变量i的值" + i);
+            if (i == 20) {
+                new Thread(ft, "有返回值的线程").start();
+            }
+        }
+        try {
+            System.out.println("子线程的返回值：" + ft.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public Object call() throws Exception {
-        return null;
+        int i = 0;
+        for (; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+        return i;
     }
 }

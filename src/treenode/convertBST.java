308e9475ca06537c0d1c176da8e85cfb5bd2d00a
ArithmeticJia <1097197237@qq.com>
2020-06-18 21:09:03 +0800

    add convertBST

--- src/treenode/convertBST.java
+++ src/treenode/convertBST.java
@@ -1,7 +1,10 @@
 package treenode;
 
+import java.util.Stack;
+
 /**
- * 538. 把二叉搜索树转换为累加树
+ * 538. 把二叉搜索树转换为累加树，每个节点的值是当前节点+比他大的节点之和
+ * 方法一，反向中序遍历，中序遍历是左根右，反向就是右根左，同时把每个节点的值更新累加和
  */
 public class convertBST {
 
@@ -17,4 +20,30 @@ public class convertBST {
 
         return root;
     }
+
+    public TreeNode convertBST2(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (!stack.isEmpty() || node != null) {
+            /* push all nodes up to (and including) this subtree's maximum on
+             * the stack. */
+            while (node != null) {
+                stack.add(node);
+                node = node.right;
+            }
+
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+
+            /* all nodes with values between the current and its parent lie in
+             * the left subtree. */
+            node = node.left;
+        }
+
+        return root;
+    }
+
 }

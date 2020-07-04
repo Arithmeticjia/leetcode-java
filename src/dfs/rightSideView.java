f76bd5e9cef9fbb19d2f9ba3c2fc2deed129e219
ArithmeticJia <1097197237@qq.com>
2020-07-04 20:13:37 +0800

    add rightSideView.java

---
+++ src/dfs/rightSideView.java
@@ -0,0 +1,33 @@
+package dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 199. 二叉树的右视图
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ */
+public class rightSideView {
+
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        // 从根节点开始访问，根节点深度是0
+        dfs(root, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        // 先访问 当前节点，再递归地访问 右子树 和 左子树。
+        // 如果当前节点所在深度还没有出现在res里，说明在该深度下当前节点是第一个被访问的节点，因此将当前节点加入res中。
+        if (depth == res.size()) {
+            res.add(root.val);
+        }
+        depth++;
+        dfs(root.right, depth);
+        dfs(root.left, depth);
+    }
+}

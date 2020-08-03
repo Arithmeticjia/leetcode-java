edbb22722bcf24752c9adc2b4cbd30e28589329f
ArithmeticJia <1097197237@qq.com>
2020-08-03 23:07:50 +0800

    add leetcode problems

---
+++ src/offer/addStrings.java
@@ -0,0 +1,25 @@
+package offer;
+
+/**
+ * 415. 字符串相加
+ */
+public class addStrings {
+
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder("");
+        // 从低位开始
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+        while(i >= 0 || j >= 0){
+            int n1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int n2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int tmp = n1 + n2 + carry;
+            carry = tmp / 10;
+            res.append(tmp % 10);
+            i--; j--;
+        }
+        if(carry == 1) {
+            res.append(1);
+        }
+        return res.reverse().toString();
+    }
+}

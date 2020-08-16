9afabc85586a9c9b5a3bb56236a182d0de1391f2
ArithmeticJia <1097197237@qq.com>
2020-08-17 00:42:13 +0800

    add leetcode problems

---
+++ src/NowcodeHuaWei/ScannerDemo.java
@@ -0,0 +1,20 @@
+package NowcodeHuaWei;
+
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // 从键盘接收数据
+
+        // nextLine方式接收字符串
+        System.out.println("nextLine方式接收：");
+        // 判断是否还有输入
+        while(scan.hasNextLine()){
+            System.out.print("1");
+        }
+        scan.close();
+    }
+}
+

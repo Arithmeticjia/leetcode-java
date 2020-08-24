416290a64d5c530779ee705397d1b0da38a55748
ArithmeticJia <1097197237@qq.com>
2020-08-24 13:51:38 +0800

    add leetcode problems

--- src/HelloHashMap/HiHashMap.java
+++ src/HelloHashMap/HiHashMap.java
@@ -1,8 +1,23 @@
 package HelloHashMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HiHashMap {
 
     public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1", "Value1");
+        map.put("2", "Value2");
+        map.put("3", "Value3");
 
+        for(String key:map.keySet()){
+            System.out.print(map.get(key));
+        }
+
+        for(Map.Entry<String, String> entry:map.entrySet()){
+            System.out.print(entry.getKey());
+            System.out.print(entry.getValue());
+        }
     }
 }

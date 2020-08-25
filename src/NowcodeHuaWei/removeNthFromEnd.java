75334d168340e7ecdd3eceef3b59324501c49e9b
ArithmeticJia <1097197237@qq.com>
2020-08-25 14:28:18 +0800

    add leetcode problems

---
+++ src/NowcodeHuaWei/removeNthFromEnd.java
@@ -0,0 +1,32 @@
+package NowcodeHuaWei;
+
+/**
+ * 删除链表的倒数第n个节点
+ **/
+public class removeNthFromEnd {
+
+    public ListNode removeNthFromEnd (ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(n-- > 1){
+            fast = fast.next;
+        }
+
+        ListNode pre = null;
+        while(fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        if(pre != null) {
+            pre.next = slow.next;
+        } else {
+            //考虑删除头节点的情况
+            head = head.next;
+        }
+        return head;
+
+    }
+}

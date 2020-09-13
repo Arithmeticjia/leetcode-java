8cda4b41bd30a90e52b61686bb046dfc423a0db6
ArithmeticJia <1097197237@qq.com>
2020-09-13 09:55:13 +0800

    add leetcode problems

--- src/errors/stackoverflowerror.java
+++ src/errors/stackoverflowerror.java
@@ -2,6 +2,16 @@ package errors;
 
 /**
  * java.lang.StackOverflowError
+ *
+ * 栈溢出错误
+ *
+ * 执行main函数会创建一个线程，同时创建一个虚拟机栈（栈内存）
+ *
+ * 调用statck.recursion()时，recursion()进行压栈操作，recursion()运行期数据的数据集(批注1)保存到栈帧1(Stack Frame)(如果main方法里面调用多个方法，会执行多个压栈操作)。
+ *
+ * recursion()递归调用时，都会产生一个新的栈帧区块，这时就会连续的产生新的栈帧区块
+ *
+ * 当栈内存超过系统配置的栈内存-Xss:2048(批注2)，就会出现java.lang.StackOverflowError异常。这也是为什么对于需要谨慎使用递归调用的原因！
  */
 public class stackoverflowerror {
 

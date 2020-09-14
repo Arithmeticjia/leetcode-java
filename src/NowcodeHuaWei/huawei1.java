66f55b64cd3aa4802d6c95dd52f6156700f672eb
ArithmeticJia <1097197237@qq.com>
2020-09-14 19:30:50 +0800

    add leetcode problems

--- src/NowcodeHuaWei/huawei1.java
+++ src/NowcodeHuaWei/huawei1.java
@@ -3,7 +3,7 @@ package NowcodeHuaWei;
 import java.util.Scanner;
 
 /**
- *
+ * https://www.cnblogs.com/cstdio1/p/13605803.html
  * 幼儿园小朋友站成一列，按位置1，2，3...顺序编号，每个小朋友都拿了若干糖果，请找出三位小朋友
  * 他们拿着相同颜色的糖果，且他们拿的糖果的总数不少于其他任何三位小朋友
  * 糖果只有两种颜色，1是红色，2是蓝色
@@ -20,9 +20,9 @@ import java.util.Scanner;
  * 3 4 6（三个小朋友的编号）
  * 2（糖果颜色）
  * 15（三个人拿的总数）
- *
+ *a
  * 思路
- * 1、两个Arrylist分别存储颜色为1.2的数据，上面讲的Arrylist里面还是个Arrylist<Integer>存糖果数量和编号，
+ * 1、两个ArrayList分别存储颜色为1.2的数据，上面讲的ArrayList里面还是个ArrayList<Integer>存糖果数量和编号，
  * 2、判断两个list的数量是否有一个大于等于3，没有就输出null，不然就分别对两个颜色的list排序，按照糖果数量优先，从大到小，编号在后，从小到大。
  * 3、如果任意一个颜色的list.size达到3，就取前三个，求和sum，并记录编号。4、 最后比较两个颜色的sum，输出大的那个
  */

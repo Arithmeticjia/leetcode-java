5906e7fe087f88f0afef48fb1ce0adb91fdd3c4f
ArithmeticJia <1097197237@qq.com>
2020-09-17 10:51:37 +0800

    add leetcode problems

---
+++ src/AlwaysTest/HelloTestNew.java
@@ -0,0 +1,38 @@
+package AlwaysTest;
+
+import threads.OverrideRunnable;
+
+/**
+ * 可重入锁
+ */
+public class HelloTestNew implements Runnable{
+
+    int i = 0;
+    int j = 0;
+    @Override
+    public void run() {
+        for(int j = 0;j < 1000000;j++){
+
+            //this,当前实例对象锁
+            synchronized(this){
+                i++;
+                increase();//synchronized的可重入性
+            }
+        }
+    }
+
+    public synchronized void increase(){
+        j++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        HelloTestNew helloTestNew = new HelloTestNew();
+        Thread t1 = new Thread(helloTestNew);
+        Thread t2 = new Thread(helloTestNew);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println(helloTestNew.i);
+    }
+}

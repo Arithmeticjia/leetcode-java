16386e528ddb957f45a598930a9902c92fe8b1a8
ArithmeticJia <1097197237@qq.com>
2020-10-14 15:37:51 +0800

    add leetcode problems

---
+++ src/atomic/AtomicIntegerTest.java
@@ -0,0 +1,35 @@
+package atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest {
+
+    private static final int THREADS_CONUT = 20;
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void increase() {
+        count.incrementAndGet();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[THREADS_CONUT];
+        for (int i = 0; i < THREADS_CONUT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 1000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+
+        System.out.println(count);
+    }
+
+}

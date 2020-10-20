fc455703a97e2c186b820f9f899af3beb9f73223
ArithmeticJia <1097197237@qq.com>
2020-10-20 09:21:37 +0800

    add leetcode problems

---
+++ src/bishi2020/pathSum.java
@@ -0,0 +1,27 @@
+package bishi2020;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class pathSum {
+
+    LinkedList<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        recur(root, sum);
+        return res;
+    }
+    void recur(TreeNode root, int tar) {
+        if(root == null) {
+            return;
+        }
+        path.add(root.val);
+        tar -= root.val;
+        if(tar == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList(path));
+        }
+        recur(root.left, tar);
+        recur(root.right, tar);
+        path.removeLast();
+    }
+}
